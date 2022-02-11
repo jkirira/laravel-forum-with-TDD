3c438a08af8f441e8fb5a373b754cf2b91baad37
jkirira <jkirira@cytonn.com>
2022-02-11 18:08:57 +0300

    Episode 2

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/threads', 'ThreadsController@index');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
+Route::get('/threads', 'ThreadsController@index');
+Route::get('/threads/{thread}', 'HomeController@index');
