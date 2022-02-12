e06f3ca2e4a832a2df178bb3c34ed27125473afa
jkirira <jkirira@cytonn.com>
2022-02-12 12:27:14 +0300

    Episode 6 and 7

--- tests/Feature/ParticipateInForumTest.php
+++ tests/Feature/ParticipateInForumTest.php
@@ -1,40 +1,40 @@
 <?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ParticipateInForumTest extends TestCase
+{
+    use DatabaseMigrations;
+
+   /** @test  */
+
+    function unauthenticated_user_may_not_add_replies()
+    {
+        $this->expectException('Illuminate\Auth\AuthenticationException');
 //
-//namespace Tests\Feature;
+//        $thread = factory('App\Thread')->create();
 //
-//use Illuminate\Foundation\Testing\DatabaseMigrations;
-//use Tests\TestCase;
-//use Illuminate\Foundation\Testing\WithFaker;
-//use Illuminate\Foundation\Testing\RefreshDatabase;
-//
-//class ParticipateInForumTest extends TestCase
-//{
-//    use DatabaseMigrations;
-//
-//   /** @test  */
-//
-//    function unauthenticated_user_may_not_add_replies()
-//    {
-//        $this->expectException('Illuminate\Auth\AuthenticationException');
-////
-////        $thread = factory('App\Thread')->create();
-////
-////        $reply = factory('App\Reply')->create();
-////        $this->post($thread->path().'/replies', $reply->toArray());
-//        $this->post('/threads/1/replies', []);
-//    }
-//
-//    function an_authenticated_user_may_participate_in_forum_threads()
-//    {
-//        $user = create("App\User");
-//        $this->be($user);
-//
-//        $thread = create('App\Thread');
-//
-//        $reply = make('App\Reply');
-//        $this->post('/threads/'.$thread->id.'/replies', $reply->toArray());
-//
-//        $this->get($thread->path())
-//            ->assertSee($reply->body);
-//    }
-//}
+//        $reply = factory('App\Reply')->create();
+//        $this->post($thread->path().'/replies', $reply->toArray());
+        $this->post('/threads/1/replies', []);
+    }
+
+    function an_authenticated_user_may_participate_in_forum_threads()
+    {
+        $user = create("App\User");
+        $this->be($user);
+
+        $thread = create('App\Thread');
+
+        $reply = make('App\Reply');
+        $this->post('/threads/'.$thread->id.'/replies', $reply->toArray());
+
+        $this->get($thread->path())
+            ->assertSee($reply->body);
+    }
+}
