b87083ac42b9a5f4f919c2a97f0684aaad497a43
jkirira <jkirira@cytonn.com>
2022-02-12 13:04:33 +0300

    Episode 8

--- tests/Feature/CreateThreadsTest.php
+++ tests/Feature/CreateThreadsTest.php
@@ -21,6 +21,14 @@ class CreateThreadsTest extends TestCase
         $this->post('/threads', $thread->toArray());
     }
 
+    /** @test  */
+    function guests_cannot_see_the_create_thread_page()
+    {
+        $this->withExceptionHandling()
+            ->get('threads/create')
+            ->assertRedirect('/login');
+    }
+
     /** @test */
     function an_authenticated_user_can_create_forum_threads()
     {
