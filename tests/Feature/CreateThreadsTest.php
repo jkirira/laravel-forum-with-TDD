8b50e90c52b6d96ef6826b780a094642a331fac7
jkirira <jkirira@cytonn.com>
2022-02-12 17:07:49 +0300

    Episode 10

--- tests/Feature/CreateThreadsTest.php
+++ tests/Feature/CreateThreadsTest.php
@@ -47,13 +47,55 @@ class CreateThreadsTest extends TestCase
     {
         $this->signIn();
 
-        $thread = create('App\Thread');
+        $thread = make('App\Thread');
 
-        $this->post('/threads', $thread->toArray());
+        $response = $this->post('/threads', $thread->toArray());
 
-        $this->get( $thread->path() )
+        $this->get( $response->headers->get('Location'))
                 ->assertSee($thread->title)
                 ->assertSee($thread->body);
+    }
+
+    /** @test */
+    function a_thread_requires_a_title()
+    {
+        $this->publishThread(['title' => null])
+            ->assertSessionHasErrors('title');
+
+//        $this->withExceptionHandling()->signIn();
+//        $thread = make('App\thread', ['title' => null]);
+//        $this->post('/threads', $thread->toArray())
+//            ->assertSessionHasErrors('title');
+    }
+
+    /** @test */
+    function a_thread_requires_a_body()
+    {
+        $this->publishThread(['body' => null])
+            ->assertSessionHasErrors('body');
+
+    }
+
+    /** @test */
+    function a_thread_requires_a_valid_channel()
+    {
+        factory('App\Channel', 2)->create();
+
+        $this->publishThread(['channel_id' => null])
+            ->assertSessionHasErrors('channel_id');
+
+        $this->publishThread(['channel_id' => 999])
+            ->assertSessionHasErrors('channel_id');
+    }
+
+
+    function publishThread($overrides=null)
+    {
+        $this->withExceptionHandling()->signIn();
+
+        $thread = make('App\Thread', $overrides ?: []);
+
+        return $this->post('/threads', $thread->toArray());
     }
 
 }
