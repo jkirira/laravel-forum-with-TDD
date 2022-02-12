b2f09f89ed87cd70c2a3111a36165e3252603b7c
jkirira <jkirira@cytonn.com>
2022-02-12 18:17:31 +0300

    Episode 11

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,6 +37,7 @@
                     <!-- Left Side Of Navbar -->
                     <ul class="nav navbar-nav">
                         <li><a href="/threads">All Threads</a></li>
+                        <li><a href="/threads/create">New Thread</a></li>
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                 Channels
