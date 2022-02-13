017462b1569766f1abf0fa723e79b16bbf88eb71
jkirira <jkirira@cytonn.com>
2022-02-13 14:32:17 +0300

    Episode 16

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-md-8 col-md-offset-2">
+            <div class="col-md-8">
                 <div class="panel panel-default">
                     <div class="panel-heading">
                         <a href="#">{{ $thread->creator->name }}</a> posted:  {{ $thread->title }}
@@ -13,34 +13,43 @@
                         {{ $thread->body }}
                     </div>
                 </div>
+
+                @foreach($replies as $reply)
+                    @include('threads.reply')
+                @endforeach
+
+                {{ $replies->links }}
+
+                @if(auth()->check())
+                    <form method="POST" action="{{ $thread->path().'/replies' }}">
+                        {{ csrf_field() }}
+                        <div class="form-group">
+                            <textarea name="body" id="body" class="form-control" placeholder="Have something to say?" rows = 5>
+                            </textarea>
+                        </div>
+
+                        <button type="submit" class="btn btn-default">Post</button>
+
+                    </form>
+
+                @else
+                    <p class="text-center">Please <a href=" {{ route('login') }}">sign in</a> to participate in this discussion</p>
+                @endif
+
             </div>
-        </div>
-    </div>
 
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            @foreach($thread->replies as $reply)
-                @include('threads.reply')
-            @endforeach
-        </div>
-    </div>
+            <div class="col-md-4">
 
-    @if(auth()->check())
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <form method="POST" action="{{ $thread->path().'/replies' }}">
-                {{ csrf_field() }}
-                <div class="form-group">
-                    <textarea name="body" id="body" class="form-control" placeholder="Have something to say?" rows = 5>
-                    </textarea>
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        This thread was published at {{ $thread->created_at->diffForHumans() }} by
+                        <a href="#"> {{ $thread->creator->name}}</a> and currently has {{ $thread->replies_count }} {{ str_plural('comment', $thread->replies_count) }}.
+                    </div>
                 </div>
 
-                <button type="submit" class="btn btn-default">Post</button>
+            </div>
 
-            </form>
         </div>
     </div>
-    @else
-        <p class="text-center">Please <a href=" {{ route('login') }}">sign in</a> to participate in this discussion</p>
-    @endif
+
 @endsection
