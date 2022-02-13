c46700ef49fdd7d90e74deae70f25c9edd16c057
jkirira <jkirira@cytonn.com>
2022-02-13 18:11:34 +0300

    Episode 18

---
+++ app/Http/Controllers/FavouritesController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Favourite;
+use App\Reply;
+use Illuminate\Http\Request;
+
+class FavouritesController extends Controller
+{
+    public function __construct()
+    {
+       $this->middleware('auth');
+    }
+
+    public function store(Reply $reply)
+    {
+        return $reply->favourite();
+//        Favourite::create([
+//            'user_id' => auth()->id(),
+//            'favourited_id' => $reply->id,
+//            'favourited_type' => get_class($reply),
+//        ]);
+    }
+}
--- app/Reply.php
+++ app/Reply.php
@@ -11,4 +11,17 @@ class Reply extends Model
     public function owner(){
        return $this->belongsTo(User::class, 'user_id');
     }
+
+    public function favourites(){
+        return $this->morphMany(Favourite::class, 'favourited');
+    }
+
+    public function favourite(){
+        $attributes = ['user_id' => auth()->id()];
+
+        if( !$this->favourites()->where($attributes)->exists() ){
+            return $this->favourites()->create($attributes);
+        }
+    }
+
 }
