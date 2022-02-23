9b8bcf2627041765f9eabcc29681c6960ed9fc49
jkirira <jkirira@cytonn.com>
2022-02-24 00:28:22 +0300

    Episode 32

--- resources/views/threads/reply.blade.php
+++ resources/views/threads/reply.blade.php
@@ -48,10 +48,17 @@
                     Edit
                 </button>
 
-                <button class="btn btn-xs mr-1" @click="destroy">
-                    Delete
-                </button>
+{{--                <button class="btn btn-xs mr-1" @click="destroy">--}}
+{{--                    Delete--}}
+{{--                </button>--}}
 
+                <form method="POST" action="/replies/{{$reply->id}}">
+                    {{ csrf_field() }}
+                    {{ method_field('DELETE') }}
+                    <button type="submit" class="btn btn-danger btn-xs">
+                        delete
+                    </button>
+                </form>
             </div>
         @endcan
 
