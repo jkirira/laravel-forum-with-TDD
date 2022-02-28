d775c1273535671524867e5ba6f27ae41a28d88b
jkirira <jkirira@cytonn.com>
2022-02-28 21:35:13 +0300

    Episode 59

--- tests/Unit/ReplyTest.php
+++ tests/Unit/ReplyTest.php
@@ -6,7 +6,7 @@ use Carbon\Carbon;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Foundation\Testing\RefreshDatabase;
+
 
 class ReplyTest extends TestCase
 {
@@ -19,7 +19,7 @@ class ReplyTest extends TestCase
     }
 
     /** @test */
-    function knows_if_it_was_just_published(){
+    function it_knows_if_it_was_just_published(){
         $reply = create('App\Reply');
 
         $this->assertTrue($reply->wasJustPublished());
@@ -32,10 +32,24 @@ class ReplyTest extends TestCase
     /** @test  */
     function it_can_detect_all_mentioned_users_in_the_body()
     {
-        $reply = create('App\Reply', [
+        $reply = new \App\Reply([
             'body' => '@JaneDoe wants to talk to @JohnDoe'
         ]);
 
         $this->assertEquals(['JaneDoe', 'JohnDoe'], $reply->mentionedUsers());
+    }
+
+    /** @test */
+    function it_wraps_mentioned_usernames_in_the_body_within_anchor_tags()
+    {
+        $reply = new \App\Reply([
+            'body' => 'Hello @JaneDoe.'
+        ]);
+
+        $this->assertEquals(
+            'Hello <a href="/profiles/JaneDoe">@JaneDoe</a>.',
+            $reply->body
+        );
+
     }
 }
